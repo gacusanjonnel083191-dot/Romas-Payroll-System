b65351faa3393bb7d668180cd29ec415569db8a1
gacusanjonnel083191-dot <gacusanjonnel083191@gmail.com>
2026-08-24 18:45:00 +0800

    Enable approval processing timeout guard

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import './cashAdvanceCompactLayout.css'
 import App from './App.jsx'
 import { installCashAdvanceCompactLayout } from './cashAdvanceCompactLayout.js'
+import { installApprovalProcessingGuard } from './approvalProcessingGuard.js'
+
+installApprovalProcessingGuard()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
