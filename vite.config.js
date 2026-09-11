165559d14f51d043d7be3976aa85363a5d1a6841
gacusanjonnel083191-dot <gacusanjonnel083191@gmail.com>
2026-09-11 12:44:08 +0800

    Enable 10 AM reseller auto-order UI

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { productionForecastPrintInvariant } from './vite.production-forecast-print-invariant.js'
 import { resellerManualCopyLastOrder } from './vite.reseller-manual-copy-last-order.js'
+import { resellerAutoOrder10amInvariant } from './vite.reseller-auto-order-10am-invariant.js'
 
 export default defineConfig({
-  plugins: [resellerManualCopyLastOrder(), productionForecastPrintInvariant(), react()],
+  plugins: [resellerAutoOrder10amInvariant(), resellerManualCopyLastOrder(), productionForecastPrintInvariant(), react()],
 })
